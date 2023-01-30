02dda61058848f42357d226bb157486bdcb06899
ramakantaswale001 <ramakantaswale001@gmail.com>
2023-01-30 20:33:18 +0530

    changes by ram

---
+++ src/bankApplication/Bank.java
@@ -0,0 +1,16 @@
+package bankApplication;
+
+public interface Bank {
+
+	void createAccount();
+	
+	void viewAccount();
+	
+	void withdraw();
+	
+	void deposit();
+	
+	void update();
+	
+	void exit();
+}
---
+++ src/bankApplication/TCS.java
@@ -0,0 +1,41 @@
+package bankApplication;
+
+public class TCS implements Bank{
+
+	@Override
+	public void createAccount() {
+		
+		
+	}
+
+	@Override
+	public void viewAccount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
